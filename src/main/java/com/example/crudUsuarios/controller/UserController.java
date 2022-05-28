997828e4a28100725e19e2987a329de3a223a149
Jademir de Moura <jademir.moura@vinta.com.br>
2022-05-27 21:10:46 -0300

    add putmapping

--- src/main/java/com/example/crudUsuarios/controller/UserController.java
+++ src/main/java/com/example/crudUsuarios/controller/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,6 +31,11 @@ public class UserController {
     @PostMapping
     public Usuario save(@RequestBody Usuario newUser) {
         return userRepo.save(newUser);
+    }
+
+    @PutMapping
+    public Usuario update(@RequestBody Usuario updateUser) {
+        return userRepo.save(updateUser);
     }
 
 
