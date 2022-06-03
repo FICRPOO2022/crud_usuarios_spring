2330095ba5133d0107aed15f0cf02d72e7b2bcc3
Jademir de Moura <jademir.moura@vinta.com.br>
2022-06-03 19:05:52 -0300

    add delete mapping

--- src/main/java/com/example/crudUsuarios/controller/UserController.java
+++ src/main/java/com/example/crudUsuarios/controller/UserController.java
@@ -6,7 +6,7 @@ import com.example.crudUsuarios.model.Usuario;
 import com.example.crudUsuarios.repository.UserRepo;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import lombok.NonNull;
 
 @RestController
 @RequestMapping(value="/users")
@@ -38,5 +37,10 @@ public class UserController {
         return userRepo.save(updateUser);
     }
 
+    @DeleteMapping
+    public String delete(@RequestBody Usuario deleteUser) {
+        userRepo.delete(deleteUser);
+        return "Usuário deletado com sucesso!";
+    }
 
 }
